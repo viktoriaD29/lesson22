9967085a4bf505d8f4d7c867a892875517ffc43d
Viktoria <doduchenko29@gmail.com>
2021-06-09 17:45:14 +0300

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+
+const logTarget = (text, color) => {
+  const eventsListElem = document.querySelector('.events-list');
+
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`
+}
+
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green')
+const logGreenP = logTarget.bind(null, 'P', 'green')
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey')
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+
+divElem.addEventListener('click', logGreyDiv, true)
+divElem.addEventListener('click', logGreenDiv);
+
+pElem.addEventListener('click', logGreyP, true);
+pElem.addEventListener('click', logGreenP);
+
+spanElem.addEventListener('click', logGreySpan, true);
+spanElem.addEventListener('click', logGreenSpan);
