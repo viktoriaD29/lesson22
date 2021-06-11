289c2d475f1015eb6959203a36d05f2f1e6d4cba
Viktoria <doduchenko29@gmail.com>
2021-06-11 15:25:23 +0300

    add

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const spanElem = document.querySelector('.rect_span');
 const logTarget = (text, color) => {
   const eventsListElem = document.querySelector('.events-list');
 
-  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`
-}
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+};
 
-const logGreenDiv = logTarget.bind(null, 'DIV', 'green')
-const logGreenP = logTarget.bind(null, 'P', 'green')
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
 const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
 
-const logGreyDiv = logTarget.bind(null, 'DIV', 'grey')
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-divElem.addEventListener('click', logGreyDiv, true)
+divElem.addEventListener('click', logGreyDiv, true);
 divElem.addEventListener('click', logGreenDiv);
 
 pElem.addEventListener('click', logGreyP, true);
@@ -24,3 +24,21 @@ pElem.addEventListener('click', logGreenP);
 
 spanElem.addEventListener('click', logGreySpan, true);
 spanElem.addEventListener('click', logGreenSpan);
+
+//для кнопк додаємо оброботчик собитий з функцією
+//в цій функції відміняється додавання цих слів в те поле
+//виходить, що ми кліку на кнопку спрацьовує функція яка відміняє обработчики собитій для блоків
+const buttonElem = document.querySelector('.remove-handlers-btn');
+
+function noLogTarget() {
+  divElem.addEventListener('click', logGreyDiv, true);
+  divElem.addEventListener('click', logGreenDiv);
+
+  pElem.addEventListener('click', logGreyP, true);
+  pElem.addEventListener('click', logGreenP);
+
+  spanElem.addEventListener('click', logGreySpan, true);
+  spanElem.addEventListener('click', logGreenSpan);
+}
+
+buttonElem.addEventListener('click', noLogTarget);
